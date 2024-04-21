ebb611e9df474fd4267e8a8c19574d7c02d02708
Carlos Rojas <161752514+Charjerohu@users.noreply.github.com>
2024-04-20 22:50:39 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='startea',
+      version='0.1.2',
+      description='import startea',
+      long_description='Making star imports possible. Or not.',
+      author='Carlos Rojas',
+      author_email='tuneritoproo@gmail.com',
+      license='Public Domain',
+      keywords='startea import',
+      platforms=['any'],
+      packages=['startea'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Quality Assurance',
+          ],
+    )
